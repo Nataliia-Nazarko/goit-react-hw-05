951318c063cefbc6f8701e7981227ae7458a6dd0
Nataliia Nazarko <90257127+Nataliia-Nazarko@users.noreply.github.com>
2024-03-20 22:25:58 +0100

    Project settings

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+//import { Suspense, lazy } from "react";
+//import { Navigate, Route, Routes } from "react-router-dom";
+
+//import Layout from "./components/Layout.jsx";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+//import { useEffect, useState } from "react";
+//import { nanoid } from "nanoid";
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
